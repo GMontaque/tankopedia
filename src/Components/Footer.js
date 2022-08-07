d03bff8083c582c53d7d6d358add0896ee1cfa66
Giulio <62805702+knightrider909@users.noreply.github.com>
2022-08-07 16:32:28 +0100

    sass and homepage update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,16 +3,25 @@ import { Link } from "react-router-dom";
 function Footer() {
 	return (
 		<footer>
-			<h1>footer title</h1>
-			<Link to="/">Navbar</Link>
-			<span>-</span>
-			<Link to="/TankBio">Homess</Link>
-			<span>-</span>
-			<Link to="/TankCrew">Link</Link>
-			<span>-</span>
-			<Link to="/Ricochet">Action 1</Link>
-			<span>-</span>
-			<Link to="/DeepDive">Action 2</Link>
+			<div className="row">
+				<h1>footer title</h1>
+				<Link to="/">Navbar</Link>
+				<span>-</span>
+				<Link to="/TankBio">Homess</Link>
+				<span>-</span>
+				<Link to="/TankCrew">Link</Link>
+				<span>-</span>
+				<Link to="/Ricochet">Action 1</Link>
+				<span>-</span>
+				<Link to="/DeepDive">Action 2</Link>
+			</div>
+			<div className="row">
+				<a href="www.google.com">
+					<i class="fab fa-discord fa-lg  "></i>
+				</a>
+
+				<i class="fab fa-discord fa-lg  "></i>
+			</div>
 		</footer>
 	);
 }
